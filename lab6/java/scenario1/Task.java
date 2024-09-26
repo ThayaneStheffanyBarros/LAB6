6392c64d124fcc9b9990ddc7b1d5c68443cca165
igor-correia <igor.correia.silva@ccc.ufcg.edu.br>
2024-09-26 17:09:40 -0300

    adicionando tempo de execução

--- lab6/java/scenario1/Task.java
+++ lab6/java/scenario1/Task.java
@@ -2,16 +2,21 @@ import java.util.Random;
 
 public class Task {
     long id;
+    long tempoInicial;
+    long duracao;
 
     public Task(long id) {
         this.id = id;
+        this.tempoInicial = System.currentTimeMillis();
     }
 
     public void execute() {
         try {
+            
             // generating a number between 1000 and 15000
             long execDuration = 1000 + (long) (new Random().nextFloat() * (15000 - 1000));
             Thread.sleep(execDuration);
+            this.duracao = System.currentTimeMillis() - this.tempoInicial;
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
